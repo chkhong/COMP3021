64cc438889db48058e967d64fd85d8960d1cc35a
chkhong <hckj1999@gmail.com>
2022-03-18 22:28:57 +0800

    lab 4

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -1,14 +1,40 @@
 package base;
 
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class NoteBook {
+public class NoteBook implements Serializable{
     private ArrayList<Folder> folders;
+
+    private static final long serialVersionUID = 1L;
 
     public NoteBook() {
         this.folders = new ArrayList<Folder>();
+    }
+
+    public NoteBook(String file) {
+        this.folders = new ArrayList<Folder>();
+
+        FileInputStream fis = null;
+        ObjectInputStream in = null;
+
+        try {
+            fis = new FileInputStream(file);
+            in = new ObjectInputStream(fis);
+
+            NoteBook notebook = (NoteBook) in.readObject();
+
+            this.folders = notebook.getFolders();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public boolean createTextNote(String folderName, String title) {
@@ -71,5 +97,20 @@ public class NoteBook {
             result.addAll(f.searchNotes(keywords));
         }
         return result;
+    }
+
+    public boolean save(String file) {
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(file);
+            out = new ObjectOutputStream(fos);
+            out.writeObject(this);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,5 +1,14 @@
 package base;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class TextNote extends Note {
     String content;
 
@@ -12,7 +21,43 @@ public class TextNote extends Note {
         this.content = content;
     }
 
+    public TextNote(File f) {
+        super(f.getName());
+        this.content = getTextFromFile(f.getAbsolutePath());
+    }
+
     public String getContent() {
         return content;
+    }
+
+    private String getTextFromFile(String absolutePath) {
+        String result = "";
+        BufferedReader br = null;
+        
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(absolutePath)));
+
+            result = br.readLine();
+            br.close();
+        } catch (FileNotFoundException e){
+            System.out.println("File not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public void exportTextToFile(String pathFolder) {
+        BufferedWriter bw = null;
+        File file = new File(pathFolder + File.separator + this.getTitle().replaceAll(" ", "_") + ".txt");
+
+        try {
+            bw = new BufferedWriter(new FileWriter(file.getAbsolutePath()));
+            bw.write(content);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
